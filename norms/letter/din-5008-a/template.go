aeffd864d995de2a5e8f26827c1f17ac6a4b4755
Felix Piasta <felix.piasta@stud.htwk-leipzig.de>
2023-07-20 18:03:54 +0200

    add generic page numbering

--- norms/letter/din-5008-a/template.go
+++ norms/letter/din-5008-a/template.go
@@ -187,6 +187,10 @@ func Footer(content func(maxFooterHeight float64) (footerStartY float64), pdfGen
 }
 
 func PageNumbering(pdfGen *generator.PDFGenerator, footerStartY float64) {
+	PageNumberingCustom("Seite", pdfGen, footerStartY)
+}
+
+func PageNumberingCustom(prefixText string, pdfGen *generator.PDFGenerator, footerStartY float64) {
 	if pdfGen.GetTotalNumber() == 1 {
 		// if pdf has only one page, no page number is required by DIN 5008 A
 		return
@@ -201,7 +205,7 @@ func PageNumbering(pdfGen *generator.PDFGenerator, footerStartY float64) {
 		pdfGen.GoToPage(i)
 		pdfGen.SetUnsafeCursor(BodyStopX, footerStartY-MarginPageNumberY)
 		pdfGen.PreviousLine(BodyStopX)
-		text := fmt.Sprintf("Seite %d von %d", i, pages)
+		text := fmt.Sprintf("%s %d von %d", prefixText, i, pages)
 		pdfGen.PrintPdfText(text, "", "R")
 	}
 
